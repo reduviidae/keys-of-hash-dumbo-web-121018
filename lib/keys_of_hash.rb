322c3d018c620dfbb0443abe51e967ec395cfbfc
Wil Cook <wil.cook.is@gmail.com>
2018-11-27 23:07:05 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
+require "pry"
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(arguments, *more_args)
+    # binding.pry
+    array = []
+    self.map do |k, v|
+      if v == arguments || v == more_args
+        array.push(k)
+      end
+    end
+    array
   end
 end
