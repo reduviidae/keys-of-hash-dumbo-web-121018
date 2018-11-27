0697fd94e052230656d380ffa799bb728a0d6c62
Wil Cook <wil.cook.is@gmail.com>
2018-11-27 23:22:04 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -5,7 +5,7 @@ class Hash
     # binding.pry
     array = []
     self.map do |k, v|
-      if v == arguments || more_argsinclude?(v)
+      if v == arguments || more_args.include?(v)
         array.push(k)
       end
     end
